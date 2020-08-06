dd4b5d2b1229179ea5a5d581fecc033ed0c34286
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-06 16:59:46 -0400

    form component works, delete and add task functions fully functional. Next, update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import React from 'react'
-import TodoItem from './TodoItem'
 import TodoForm from './TodoForm'
 
 class TodoList extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      todos: [],
-      currentToDo: '',
-      idCount: 0
-    }
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      currentToDo: event.target.value
-    })
-  }
-
-  addItem = (event) => {
-    event.preventDefault()
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.concat(<TodoItem key={prevState.idCount} id={prevState.idCount} text={prevState.currentToDo} onClick={this.delete} />)
-      return {
-        todos: updatedTodos,
-        currentToDo: '',
-        idCount: prevState.idCount + 1
-      }
-    })
-  }
-
-  delete = (id) => {
-    console.log(id)
-    this.setState(prevState => {
-      const updatedTds = prevState.todos.filter(td => td.props.id !== id) 
-      return {
-        todos: updatedTds
-      }
-    })
-  }
 
   render() {
     return (
       <div className='container'>
-        {/* <form onSubmit={this.addItem}>
-            <label htmlFor='taskName'>Task:</label>
-            <input type='text' name='taskName' value={this.state.currentToDo} placeholder='Enter New Task' onChange={this.handleChange}/>
-            <button type='submit'>Add Task</button>
-        </form> */}
-        <TodoForm onSubmit={this.addItem} value={this.state.currentToDo} onChange={this.handleChange}/>
-        <div className='todo-list'>
-          {this.state.todos}
-        </div>
+          <TodoForm />
       </div>
     )
   }
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,15 +1,57 @@
 import React from 'react'
+import TodoItem from './TodoItem'
 
-const TodoForm = (props) => {
-    return (
-        <div className='todo-form'>
-            <form onSubmit={()=> props.onSubmit}>
-                <label htmlFor='taskName'>Task:</label>
-                <input type='text' name='taskName' value={props.currentToDo} placeholder='Enter New Task' onChange={() => props.onChange}/>
-                <button type='submit'>Add Task</button>
-            </form>
-        </div>
-    )
+class TodoForm extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            currentToDo: '',
+            todos: [],
+            idCount: 0
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+          currentToDo: event.target.value
+        })
+    }
+    
+    addItem = (event) => {
+        event.preventDefault()
+        this.setState(prevState => {
+        const updatedTodos = prevState.todos.concat(<TodoItem key={prevState.idCount} id={prevState.idCount} text={prevState.currentToDo} onClick={this.delete} />)
+            return {
+                todos: updatedTodos,
+                currentToDo: '',
+                idCount: prevState.idCount + 1
+            }
+        })
+    }
+
+    delete = (id) => {
+        this.setState(prevState => {
+        const updatedTds = prevState.todos.filter(td => td.props.id !== id) 
+            return {
+                todos: updatedTds
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='todo-form'>
+                <form onSubmit={this.addItem}>
+                    <h3>- Task -</h3>
+                    <input type='text' value={this.state.currentToDo} placeholder='Enter New Task' onChange={this.handleChange}/>
+                    <button type='submit'>Add Task</button>
+                </form>
+                <div className='todo-list'>
+                    {this.state.todos}
+                </div>
+            </div>
+        )
+    }  
 }
 
 export default TodoForm
