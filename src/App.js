a30e0cbd3e1d02395b1ca35c9acd68ecc2cf436f
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-06 12:55:42 -0400

    to do list now populating items

--- src/App.js
+++ src/App.js
@@ -7,17 +7,41 @@ class TodoList extends React.Component {
       todos: [],
       currentToDo: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      currentToDo: event.target.value
+    })
+  }
+
+  addItem(event) {
+    event.preventDefault()
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.concat(this.state.currentToDo)
+      return {
+        todos: updatedTodos,
+        currentToDo: ''
+      }
+    })
   }
 
   render() {
+    const todoListElements = this.state.todos.map(td => <li key={this.state.todos.indexOf(td)}>{td}</li>)
     return (
       <div className='container'>
-        <form>
+        <form onSubmit={this.addItem}>
             <label htmlFor='taskName'>Task:</label>
-            <input type='text' name='taskName' placeholder='Enter New Task'/>
-            <button type='submit' >Add Task</button>
+            <input type='text' name='taskName' value={this.state.currentToDo} placeholder='Enter New Task' onChange={this.handleChange}/>
+            <button type='submit'>Add Task</button>
         </form>
+        <ul>
+          {todoListElements}
+        </ul>
       </div>
+
     )
   }
 }
