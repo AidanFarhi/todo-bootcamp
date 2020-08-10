9ebcd5164375c5dc1d53dc487ea705961cb80958
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-09 23:23:33 -0400

    styling overhaul

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -40,17 +40,29 @@ class TodoItem extends React.Component{
         }
     }
 
-    // dotsClicked = () => {
-
-    // }
+    dotsClicked = () => {
+        this.setState(prevState => {
+            return {
+                dotsClicked: !prevState.dotsClicked
+            }
+        })
+    }
     
     render() {
         return (
             <div className='td-item' id={this.state.id}>
                 <div id='item-text-and-button'>
                     <h3 className='td-text'>{this.state.text}</h3>
-                    <button id='dots' onClick={this.edit}><span id='button-dots'>...</span></button>
+                    <button id='dots' onClick={this.dotsClicked}><span id='button-dots'>...</span></button>
                 </div>
+                {/* This is where the edit and delete button will go */
+                this.state.dotsClicked ? 
+                    <div className='edit-delete-btns'>
+                        <button id='delete-btn' className='btn' onClick={() => this.state.onClick(this.state.id)}>delete</button>
+                        <br/>
+                        <button id='edit-btn' className='btn' onClick={this.edit}>edit</button>
+                    </div> 
+                : null}
                 {this.state.editClicked ?
                     <div className='update-form'>
                         <span className='form-top'></span>
@@ -60,8 +72,7 @@ class TodoItem extends React.Component{
                             placeholder='Edit Task' 
                             onChange={this.handleChange}/>
                             <button className='btn' type='submit'>save</button>
-                            <button id='delete-btn' className='btn' 
-                                    onClick={() => this.state.onClick(this.state.id)}>delete</button>
+                            
                         </form>
                     </div>
                 : null}
